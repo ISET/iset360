84ab249ba062ef79675fdc68327485778f6e7589
tlian7 <tlian@stanford.edu>
2018-04-05 16:57:28 -0700

    Put all scenes on RDT. Adjusted get command to download directly from RDT.

--- utilities/selectBitterliScene.m
+++ utilities/selectBitterliScene.m
@@ -3,42 +3,60 @@ function [pbrtFile,rigOrigin] = selectBitterliScene(sceneName)
 %rig locations here. These scenes are all from Benedikt Bitterli
 %(https://benedikt-bitterli.me/resources/) but modified to include more
 %lights and to be compatible with the pbrt2ISET parser. The rig origin
-%locations were chosen manually by me; they tend to be near the center of
+%locations were chosen manually; they are near the center of
 %the room at roughly 5-6 ft above the ground.
 
-% Hard coded for now.
-sceneDir = fullfile(rigRootPath,'data','scenes');
+% We will use the remote data toolbox to download the scenes into your
+% piRootPath/data folder. If the scenes already exist, the download should
+% skip. 
+sceneDir = fullfile(piRootPath,'data');
 
-if(~exist(sceneDir,'dir'))
-    error(sprintf('Scene directory "%s" not found. \n',sceneDir))
-end
 
 switch sceneName
     case('whiteRoom')
-        pbrtFile = fullfile(sceneDir,'living-room-2','scene.pbrt');
+        sceneName = 'white-room';
         rigOrigin = [0.9476 1.3018 3.4785] + [0 0.600 0];
+        
     case('livingRoom')
-        pbrtFile = fullfile(sceneDir,'living-room','scene.pbrt');
+        sceneName = 'living-room';
         rigOrigin = [2.7007    1.5571   -1.6591];
+        
     case('bathroom')
-        pbrtFile = fullfile(sceneDir,'bathroom','scene.pbrt');
         rigOrigin = [0.3   1.667   -1.5];
+        
     case('kitchen')
-        pbrtFile = fullfile(sceneDir,'kitchen','scene.pbrt');
         rigOrigin = [0.1768    1.7000   -0.2107];
+        
     case('bathroom2')
-        pbrtFile = fullfile(sceneDir,'bathroom2','scene.pbrt');
         rigOrigin = [];
+        
     case('bedroom')
-        pbrtFile = fullfile(sceneDir,'bedroom','scene.pbrt');
         rigOrigin = [1.1854    1.1615    1.3385];
+        
     otherwise
         error('Scene not recognized.');
+end
+
+% If file does not already exist, download it.
+pbrtFile = fullfile(sceneDir,sceneName,'scene.pbrt');
+if(~exist(pbrtFile,'file'))
+    piPBRTFetch(sceneName,'deletezip',true);
+    % Check if file exists
+    if(~exist(pbrtFile,'file'))
+        error('Something went wrong when downloading the scene.')
+    else
+        % Success!
+        fprintf('PBRT scene downloaded! File is located at: %s \n',pbrtFile);
+    end
+ 
+else
+    fprintf('Scene already exists in data folder. Skipping download.\n');
 end
 
 if(isempty(rigOrigin))
     warning('Rig origin not set for this scene yet.')
 end
+
 
 end
 
